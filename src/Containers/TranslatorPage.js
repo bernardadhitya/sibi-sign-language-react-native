e0aeef2f644470ecbe35ecb02d12441affd68cf3
Bernard Adhitya <bernard.adhitya@gmail.com>
2022-01-10 01:29:32 +0700

    Add "slideshow" option on translation page

--- src/Containers/TranslatorPage.js
+++ src/Containers/TranslatorPage.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { Text, SafeAreaView, View, TextInput, Image, Dimensions } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, SafeAreaView, View, TextInput, Image, ScrollView, TouchableOpacity } from 'react-native';
 import { Fonts } from '../Constants/Fonts';
 import AppLoading from 'expo-app-loading';
 import { useFonts } from '@use-expo/font';
-import { ScrollView } from 'react-native';
 import { images } from '../Constants/Images';
 
 const TranslatorPage = () => {
   let [fontsLoaded] = useFonts(Fonts);
-  const [text, setText] = React.useState("");
+  const [text, setText] = useState("");
+  const [playing, setPlaying] = useState(false);
+  const [counterPlayed, setCounterPlayed] = useState(0);
 
   const renderTranslationCards = () => {
     return text.split('').map((letter, idx) => {
@@ -54,6 +55,93 @@ const TranslatorPage = () => {
         </View>
       )
     })
+  }
+
+  const renderTranslationSlideShow = () => {
+    const imageUrl = images[text.split('')[counterPlayed % text.length].toUpperCase()];
+    return (
+      <View
+        style={{
+          width: 200,
+          height: 220,
+          margin: 20,
+          borderRadius: 10,
+          backgroundColor: 'white',
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 1,
+          },
+          shadowOpacity: 0.22,
+          shadowRadius: 2.22,
+          elevation: 3,
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Image
+          style={{
+            width: 150,
+            height: 150,
+            resizeMode: 'stretch',
+            marginBottom: 18
+          }}
+          source={imageUrl}
+        />
+        <Text
+          style={{
+            fontFamily: 'Bold',
+            fontSize: 24
+          }}
+        >
+          {text.split('')[counterPlayed % text.length].toUpperCase()}
+        </Text>
+      </View>
+    )
+  }
+  
+  useEffect(() => {
+    const intervalID = setTimeout(() =>  {
+      setCounterPlayed(counterPlayed+1)
+    }, 2000);
+
+    return () => clearInterval(intervalID);
+  }, [counterPlayed]);
+
+  const renderTranslationSlider = () => {
+    return (
+      <View style={{ alignItems: "center"}}>
+        {
+          playing ?
+          <>{ renderTranslationSlideShow() }</> :
+          <ScrollView horizontal>
+            { renderTranslationCards() }
+          </ScrollView>
+        }
+        <TouchableOpacity
+          style={{
+            backgroundColor: 'white',
+            paddingTop: 16,
+            paddingHorizontal: 32,
+            borderRadius: 32,
+            width: '50%'
+          }}
+          onPress={() => {setCounterPlayed(0); setPlaying(!playing)}}
+        >
+          <Text
+            style={{
+              fontFamily: 'Medium',
+              fontSize: 18,
+              marginBottom: 18,
+              textAlign: 'center',
+              color: playing ? 'red' : 'black'
+            }}
+          >
+            {playing ? 'Stop' : 'Play'}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    )
   }
 
   if (!fontsLoaded) {
@@ -113,7 +201,7 @@ const TranslatorPage = () => {
           </View>
           <View
             style={{
-              marginTop: 80,
+              marginTop: 40,
               marginHorizontal: 30
             }}
           >
@@ -126,9 +214,7 @@ const TranslatorPage = () => {
             >
               Hasil terjemahan
             </Text>
-            { text.length > 0 ? <ScrollView horizontal>
-              { renderTranslationCards() }
-            </ScrollView> : <View
+            { text.length > 0 ? renderTranslationSlider() : <View
               style={{
                 flex: 1,
                 alignItems: 'center',
